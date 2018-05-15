1fdd4f57d7a85ab7f63e2c49f2009e7a56159cb7
Priya15494 <priyankakarri15@gmail.com>
2018-05-15 22:32:36 +0530

    first commit

---
+++ modal.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+     $(".modal").css("display","none");
+     
+$(".btn").click(() => {
+    $(".modal").css("display","block")
+
+});
+
+
+$(".close").click(() => {
+    $(".modal").css("display","none")
+
+});
+
+$(".close1").click(() => {
+    $(".modal").css("display","none")
+
+});
+});
